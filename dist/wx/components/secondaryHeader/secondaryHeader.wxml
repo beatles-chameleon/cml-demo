745b198d1a34f3380c38bd6e11512d6dae3982c7
sgoddon <xuguodong@didichuxing.com>
2019-01-28 22:04:16 +0800

    build

---
+++ dist/wx/components/secondaryHeader/secondaryHeader.wxml
@@ -0,0 +1,4 @@
+<view height="{{0}}" class="secondary-header  cml-base cml-view">
+  <text class="header-title  cml-base cml-text">{{title}}</text>
+  <text class="header-subtitle  cml-base cml-text">{{subtitle}}</text>    
+</view>
---
+++ dist/wx/static/js/pages/com/base/textarea/textarea.js
@@ -0,0 +1,1 @@
+var __CML__GLOBAL=require("../../../../manifest.js");__CML__GLOBAL.webpackJsonp([33],{192:function(e,t,u){u(193),u(194)},193:function(e,t){},194:function(e,t,u){Object.defineProperty(t,"__esModule",{value:!0});var n=u(0),a=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default={},t.default=a.default.createPage(t.default).getOptions()}},[192]);
