b605e25ca170d24460490750b98aa73510cdda8d
朱智恒 <zhuzhiheng@didichuxing.com>
2019-02-25 13:46:16 +0800

    gitingore  dist

--- dist/wx/components/collapse/collapse.js
+++
@@ -1,4 +0,0 @@
-var __CML__GLOBAL = require('../../static/js/manifest.js')
-__CML__GLOBAL.Component = Component;
-require('../../static/js/common.js')
-require('../../static/js/components/collapse/collapse.js')
