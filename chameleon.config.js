55a3497b982971298ad4ebc8f62e52f7a712e538
JiM-W <550116990@qq.com>
2019-07-18 16:53:26 +0800

    头条能启动

--- chameleon.config.js
+++ chameleon.config.js
@@ -3,10 +3,32 @@
 const publicPath = 'https://beatles-chameleon.github.io/cml-demo/dist';
 //设置api请求前缀
 const apiPrefix = 'https://cmljs.org';
-
+const path = require('path');
 cml.config.merge({
   cmlNpm: [
   ],
+  builtinNpmName: 'cml-tt-ui-builtin',
+  extPlatform: {
+    tt: 'cml-tt-plugin',
+  },
+  babelPath: [
+    path.join(__dirname,'node_modules/cml-tt-ui-builtin'),
+    path.join(__dirname,'node_modules/cml-tt-runtime'),
+    path.join(__dirname,'node_modules/cml-tt-api'),
+    path.join(__dirname,'node_modules/cml-tt-ui'),
+    path.join(__dirname,'node_modules/cml-tt-store'),
+    path.join(__dirname,'node_modules/cml-tt-mixins'),
+  ],
+  baseStyle:{
+    wx: true,
+    web: true,
+    weex: true,
+    alipay: true,
+    baidu: true,
+    qq: true,
+    tt:true,
+  },
+  platforms: ['tt'],
   devPort: 5556,
   templateType: 'html',
   // platforms: ["web","weex"],
---
+++ src/router.config.js
@@ -0,0 +1,241 @@
+module.exports = {
+  "mode": "hash",
+  "domain": "https://cmljs.org",
+  "routes":[
+    {
+      "url": "/",
+      "path": "/pages/index/index",
+      "name": "首页",
+      "mock": "index.php"
+    },
+    {
+      "name": "com",
+      "url": "/pages/com/com",
+      "path": "/pages/com/com",
+      "mock": "index.php"
+    },
+    {
+      "name": "api",
+      "url": "/pages/api/api",
+      "path": "/pages/api/api",
+      "mock": "index.php"
+    },
+    {
+      "name": "demo",
+      "url": "/pages/demo/demo",
+      "path": "/pages/demo/demo",
+      "mock": "index.php"
+    },
+    {
+      "name": "chooseImage",
+      "url": "/pages/api/sub-pages/chooseImage",
+      "path": "/pages/api/sub-pages/chooseImage",
+      "mock": "index.php"
+    },    
+    {
+      "name": "animation",
+      "url": "/pages/api/sub-pages/animation",
+      "path": "/pages/api/sub-pages/animation",
+      "mock": "index.php"
+    },
+    {
+      "name": "request",
+      "url": "/pages/api/sub-pages/request",
+      "path": "/pages/api/sub-pages/request",
+      "mock": "index.php"
+    },    
+    {
+      "name": "webSocket",
+      "url": "/pages/api/sub-pages/webSocket",
+      "path": "/pages/api/sub-pages/webSocket",
+      "mock": "index.php"
+    },    
+    {
+      "name": "navigate",
+      "url": "/pages/api/sub-pages/navigate",
+      "path": "/pages/api/sub-pages/navigate",
+      "mock": "index.php"
+    },
+    {
+      "name": "list",
+      "url": "/pages/com/base/list/list",
+      "path": "/pages/com/base/list/list",
+      "mock": "index.php"
+    },
+    {
+      "name": "scroller",
+      "url": "/pages/com/base/scroller/scroller",
+      "path": "/pages/com/base/scroller/scroller",
+      "mock": "index.php"
+    },
+    {
+      "name": "view",
+      "url": "/pages/com/base/view/view",
+      "path": "/pages/com/base/view/view",
+      "mock": "index.php"
+    },
+    {
+      "name": "text",
+      "url": "/pages/com/base/text/text",
+      "path": "/pages/com/base/text/text",
+      "mock": "index.php"
+    },
+    {
+      "name": "button",
+      "url": "/pages/com/base/button/button",
+      "path": "/pages/com/base/button/button",
+      "mock": "index.php"
+    },
+    {
+      "name": "input",
+      "url": "/pages/com/base/input/input",
+      "path": "/pages/com/base/input/input",
+      "mock": "index.php"
+    },
+    {
+      "name": "textarea",
+      "url": "/pages/com/base/textarea/textarea",
+      "path": "/pages/com/base/textarea/textarea",
+      "mock": "index.php"
+    },
+    {
+      "name": "image",
+      "url": "/pages/com/base/image/image",
+      "path": "/pages/com/base/image/image",
+      "mock": "index.php"
+    },
+    {
+      "name": "video",
+      "url": "/pages/com/base/video/video",
+      "path": "/pages/com/base/video/video",
+      "mock": "index.php"
+    },
+    {
+      "name": "richtext",
+      "url": "/pages/com/base/richtext/richtext",
+      "path": "/pages/com/base/richtext/richtext"
+    },
+    {
+      "name": "switch",
+      "url": "/pages/com/base/switch/switch",
+      "path": "/pages/com/base/switch/switch"
+    },
+    {
+      "name": "radio",
+      "url": "/pages/com/base/radio/radio",
+      "path": "/pages/com/base/radio/radio"
+    },
+    {
+      "name": "checkbox",
+      "url": "/pages/com/base/checkbox/checkbox",
+      "path": "/pages/com/base/checkbox/checkbox"
+    },
+    {
+      "name": "carousel",
+      "url": "/pages/com/base/carousel/carousel",
+      "path": "/pages/com/base/carousel/carousel"
+    },
+    {
+      "name": "row",
+      "url": "/pages/com/base/row/row",
+      "path": "/pages/com/base/row/row",
+      "mock": "index.php"
+    },
+    {
+      "name": "layout",
+      "url": "/pages/com/base/layout/layout",
+      "path": "/pages/com/base/layout/layout",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-dialog",
+      "url": "/pages/com/spread/c-dialog/c-dialog",
+      "path": "/pages/com/spread/c-dialog/c-dialog",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-loading",
+      "url": "/pages/com/spread/c-loading/c-loading",
+      "path": "/pages/com/spread/c-loading/c-loading",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-toast",
+      "url": "/pages/com/spread/c-toast/c-toast",
+      "path": "/pages/com/spread/c-toast/c-toast",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-tip",
+      "url": "/pages/com/spread/c-tip/c-tip",
+      "path": "/pages/com/spread/c-tip/c-tip",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-popup",
+      "url": "/pages/com/spread/c-popup/c-popup",
+      "path": "/pages/com/spread/c-popup/c-popup",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-actionsheet",
+      "url": "/pages/com/spread/c-actionsheet/c-actionsheet",
+      "path": "/pages/com/spread/c-actionsheet/c-actionsheet",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-picker",
+      "url": "/pages/com/spread/c-picker/c-picker",
+      "path": "/pages/com/spread/c-picker/c-picker",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-tab",
+      "url": "/pages/com/spread/c-tab/c-tab",
+      "path": "/pages/com/spread/c-tab/c-tab",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-refresh",
+      "url": "/pages/com/spread/c-refresh/c-refresh",
+      "path": "/pages/com/spread/c-refresh/c-refresh",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-checkbox-group",
+      "url": "/pages/com/spread/c-checkbox-group/c-checkbox-group",
+      "path": "/pages/com/spread/c-checkbox-group/c-checkbox-group",
+      "mock": "index.php"
+    },
+    {
+      "name": "c-radio-group",
+      "url": "/pages/com/spread/c-radio-group/c-radio-group",
+      "path": "/pages/com/spread/c-radio-group/c-radio-group",
+      "mock": "index.php"
+    },
+    {
+      "url": "/cml/demo/yanxuan",
+      "path": "/pages/demo/yanxuan/pages/index/index",
+      "name": "yanxuan",
+      "mock": "index.php"
+    },
+    {
+      "url": "/cml/demo/yanxuan/list",
+      "path": "/pages/demo/yanxuan/pages/list/list",
+      "name": "yanxuan_list",
+      "mock": "index.php"
+    },
+    {
+      "url": "/cml/demo/yanxuan/detail",
+      "path": "/pages/demo/yanxuan/pages/detail/detail",
+      "name": "yanxuan_detail",
+      "mock": "index.php"
+    },
+    {
+      "url": "/cml/demo/yanxuan/map",
+      "path": "/pages/demo/yanxuan/pages/map/map",
+      "name": "yanxuan_map",
+      "mock": "index.php"
+    }
+  ]
+}
