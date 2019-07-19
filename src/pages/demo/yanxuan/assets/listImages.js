da1e66b8513515fd36e1c0a580437b814d35c175
JiM-W <550116990@qq.com>
2019-07-19 17:22:59 +0800

    0.22

---
+++ src/pages/demo/yanxuan/assets/listImages.js
@@ -0,0 +1,35 @@
+modulex.exports = {
+  "code": 0,
+  "data": {
+    "topImgUrl": "https://cmljs.org/cml-demo/src/assets/images/yanxuan/4972949f269e7295a4f37e99a303553e.jpg",
+    "list":[
+      {
+        "imgUrl": "https://cmljs.org/cml-demo/src/assets/images/yanxuan/8635c42f2b3a92768b12015c491821b5.png",
+        "des": "400跟纯棉贡缎",
+        "name": "60s锦绵贡缎四件套",
+        "money": "￥400",
+        "tag":["APP特惠"]
+      },
+      {
+        "imgUrl": "https://cmljs.org/cml-demo/src/assets/images/yanxuan/4a00fd1035efe874d70d51dfc04c5cee.png",
+        "des": "少女粉润贡缎",
+        "name": "朱莉.粉唐四件套",
+        "money": "￥359",
+        "tag":["年货节特卖", "满赠"]
+      },
+      {
+        "imgUrl": "https://cmljs.org/cml-demo/src/assets/images/yanxuan/9f3c89c8b98fb06968c67edb12a195cf.png",
+        "des": "少女粉润贡缎",
+        "name": "朱莉.粉唐四件套",
+        "money": "￥359",
+        "tag":[]
+      },
+      {
+        "imgUrl": "https://cmljs.org/cml-demo/src/assets/images/yanxuan/cc863b46d890633445fb2a1354b01841.png",
+        "des": "入门享受奢华感受",
+        "name": "60s锦绵贡缎四件套",
+        "money": "￥400"
+      }
+    ]
+  }
+}
---
+++ src/pages/demo/yanxuan/assets/yanxuan.js
@@ -0,0 +1,393 @@
+
+module.exports = {
+		"errno": "0",
+		"errmsg": "",
+		"data": {
+            "result" : {
+                "banners" : [
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/630439320dae9f1ce3afef3c39721383.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/5100f0176e27a167cc2aea08b1bd11d8.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/banner-1.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/banner-8.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/banner-2.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/banner-4.jpg"
+                    },
+                    {
+                        "title" : "", 
+                        "url" : "https://cmljs.org/cml-demo/src/assets/images/yanxuan/banner-6.jpg"
+                    }
+                ],
+                "makers" : {
+                    "title" : "品牌制造商直供",
+                    "list" : [
+                        {
+                          "name" : "新秀丽制造商",
+                          "price" : "59",
+                          "state" : "上新",
+                          "bg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/ppbg-1.jpg",
+                          "url" : ""
+                        },
+                        {
+                          "name" : "MUJI制造商",
+                          "price" : "12.9",
+                          "state" : "上新",
+                          "bg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/ppbg-2.jpg",
+                          "url" : ""
+                        },
+                        {
+                          "name" : "CK制造商",
+                          "price" : "29",
+                          "state" : "上新",
+                          "bg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/ppbg-3.jpg",
+                          "url" : ""
+                        },
+                        {
+                          "name" : "Adidas制造商",
+                          "price" : "29",
+                          "bg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/75523d4274d85825ece16370cdb1693f.jpg",
+                          "url" : ""
+                        }
+                    ]
+                },
+                
+                "recommend" : {
+                    "head1" : {
+                        "tlt" : "周一周四 · 新品发布",
+                        "tltBg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/bg-new.png",
+                        "url" : ""
+                    },
+                    "goods1" : [
+                        {
+                            "tlt" : "日式和风声波式电动牙刷",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/e5474a8f4fd5748079e2ba2ead806b51.png",
+                            "info" : "进口刷毛，专利技术",
+                            "price" : "119"
+                        },
+                        {
+                            "tlt" : "小馒头 多色双肩包",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/455eee1712358dbcb2e33d54ab287808.png",
+                            "info" : "奶油色泽，小巧减龄",
+                            "price" : "79"
+                        },
+                        {
+                            "tlt" : "多功能商务双肩包",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/795884dc6d995eca9a091a6358e3634d.png",
+                            "info" : "17个功能分区，内置分层收纳",
+                            "price" : "334"
+                        },
+                        {
+                            "tlt" : "切尔西牛皮女靴",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/0e9ddf1a0ed5af78e8ec12cb9489df17.png",
+                            "info" : "经典牛皮切尔西，时尚帅气",
+                            "price" : "289"
+                        },
+                        {
+                            "tlt" : "清心花茶壶套装",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/a2a0f13385d67220b29e7a1124a361e6.png",
+                            "info" : "明亮通透，滤茶迅速",
+                            "price" : "119"
+                        },
+                        {
+                            "tlt" : "全棉色织磨毛四件套",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3e1c00ce7b49a78e645538a8c45cabcb.png",
+                            "info" : "优雅色织，温暖磨毛",
+                            "price" : "299"
+                        },
+                        {
+                            "tlt" : "黑凤梨 20寸PC膜拉链登机箱",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3108aaae80416b1cf27c3d7cc5661cea.png",
+                            "info" : "热卖9万只，网易人手1只",
+                            "price" : "185"
+                        },
+                        {
+                            "tlt" : "日式和风敞口保温杯",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3aa67fee1c7d046a09f4ce878f4485ac.png",
+                            "info" : "真空隔热，保温保冷",
+                            "price" : "32"
+                        }
+                    ],
+                    "head2" : {
+                        "tlt" : "周一周四 · 新品发布",
+                        "tltBg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/bg-new.png",
+                        "url" : ""
+                    },
+                    "goods2" : [
+                        {
+                            "tlt" : "日式和风声波式电动牙刷",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/e5474a8f4fd5748079e2ba2ead806b51.png",
+                            "info" : "进口刷毛，专利技术",
+                            "price" : "119"
+                        },
+                        {
+                            "tlt" : "小馒头 多色双肩包",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/455eee1712358dbcb2e33d54ab287808.png",
+                            "info" : "奶油色泽，小巧减龄",
+                            "price" : "79"
+                        },
+                        {
+                            "tlt" : "多功能商务双肩包",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/795884dc6d995eca9a091a6358e3634d.png",
+                            "info" : "17个功能分区，内置分层收纳",
+                            "price" : "334"
+                        },
+                        {
+                            "tlt" : "切尔西牛皮女靴",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/0e9ddf1a0ed5af78e8ec12cb9489df17.png",
+                            "info" : "经典牛皮切尔西，时尚帅气",
+                            "price" : "289"
+                        },
+                        {
+                            "tlt" : "清心花茶壶套装",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/a2a0f13385d67220b29e7a1124a361e6.png",
+                            "info" : "明亮通透，滤茶迅速",
+                            "price" : "119"
+                        },
+                        {
+                            "tlt" : "全棉色织磨毛四件套",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3e1c00ce7b49a78e645538a8c45cabcb.png",
+                            "info" : "优雅色织，温暖磨毛",
+                            "price" : "299"
+                        },
+                        {
+                            "tlt" : "黑凤梨 20寸PC膜拉链登机箱",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3108aaae80416b1cf27c3d7cc5661cea.png",
+                            "info" : "热卖9万只，网易人手1只",
+                            "price" : "185"
+                        },
+                        {
+                            "tlt" : "日式和风敞口保温杯",
+                            "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3aa67fee1c7d046a09f4ce878f4485ac.png",
+                            "info" : "真空隔热，保温保冷",
+                            "price" : "32"
+                        }
+                    ]
+                },
+
+                "goods" : [
+                    {
+                        "tlt" : "日式和风敞口保温杯",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3aa67fee1c7d046a09f4ce878f4485ac.png",
+                        "info" : "真空隔热，保温保冷",
+                        "url" : "",
+                        "price" : "32"
+                    },
+                    {
+                        "tlt" : "切尔西牛皮女靴",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/0e9ddf1a0ed5af78e8ec12cb9489df17.png",
+                        "info" : "奶油色泽，小巧减龄",
+                        "url" : "",
+                        "price" : "32"
+                    },
+                    {
+                        "tlt" : "小馒头多色双肩包",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/455eee1712358dbcb2e33d54ab287808.png",
+                        "info" : "奶油色泽，小巧减龄",
+                        "url" : "",
+                        "price" : "79"
+                    },
+                    {
+                        "tlt" : "全棉色织磨毛四件套",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3e1c00ce7b49a78e645538a8c45cabcb.png",
+                        "info" : "优雅色织，温暖磨毛",
+                        "url" : "",
+                        "price" : "299"
+                    },
+                    {
+                        "tlt" : "日式和风声波式电动牙刷",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/e5474a8f4fd5748079e2ba2ead806b51.png",
+                        "info" : "进口刷毛，专利技术",
+                        "url" : "",
+                        "price" : "119"
+                    },
+                    {
+                        "tlt" : "多功能商务双肩包",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/795884dc6d995eca9a091a6358e3634d.png",
+                        "info" : "17个功能分区，内置分层收纳",
+                        "url" : "",
+                        "price" : "334"
+                    },
+                    {
+                        "tlt" : "黑凤梨20寸PC膜拉链登机箱",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3108aaae80416b1cf27c3d7cc5661cea.png",
+                        "info" : "热卖9万只，网易人手1只",
+                        "url" : "",
+                        "price" : "185"
+                    },
+                    {
+                        "tlt" : "日式蓬软太鼓抱枕",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/ad953e16ad8c33b714e7af941ce8cd56.png",
+                        "info" : "萌趣太鼓造型 软糯轻柔回弹",
+                        "url" : "",
+                        "price" : "29"
+                    },
+                    {
+                        "tlt" : "可水洗抗菌防螨丝羽绒枕",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/a6c9e142fd008b3734c690a71a78ae5b.png",
+                        "info" : "进口防螨布，热销50万件",
+                        "url" : "",
+                        "price" : "99"
+                    },
+                    {
+                        "tlt" : "双宫茧桑蚕丝被 空调被",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/6b341648f59d0c3eb48fa81e1d2de06e.png",
+                        "info" : "一级桑蚕丝，吸湿透气柔软",
+                        "url" : "",
+                        "price" : "479"
+                    },
+                    {
+                        "tlt" : "怀抱休闲椅组合（皮款）",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/b5289125e9b55cf72e9a9623d006415e.png",
+                        "info" : "葛优躺神器皮款",
+                        "url" : "",
+                        "price" : "3999"
+                    },
+                    {
+                        "tlt" : "欧式哑光餐具套装",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/431e86c88b4a6c9f065d1d8abea6b603.png",
+                        "info" : "德化白瓷，坚实耐用",
+                        "url" : "",
+                        "price" : "189"
+                    },
+                    {
+                        "tlt" : "清新两用杯",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/431f5d142e3dd9946dc8e38c2aa3cd34.png",
+                        "info" : "办公杯优选 轻松饮茶",
+                        "url" : "",
+                        "price" : "52"
+                    },
+                    {
+                        "tlt" : "两带式男/女款拖鞋",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/7d1c130c7d80edf824e4218c6829b7c8.png",
+                        "info" : "鞋杯随脚型而变，舒适呵护",
+                        "url" : "",
+                        "price" : "69.9"
+                    }
+                ],
+                "topics" : [
+                    {
+                        "name" : "严选look",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15030393722652401.jpg"
+                    },
+                    {
+                        "name" : "严选推荐",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/d943675462a06f817d33062d4eb059f5.jpg"
+                    },
+                    {
+                        "name" : "丁磊私物推荐",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/1de4da49367dd7c01af1f7a2b23b0237.jpg"
+                    },
+                    {
+                        "name" : "挑款师推荐",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/437cc656ff529f8f84db6efc48df9bf4.png"
+                    }
+                ],
+                "articles" : [
+                    {
+                        "auther" : "严选推荐",
+                        "autherimg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3d860cbf663253590da6a64ff07f9919.png",
+                        "tlt" : "年中扫一扫，下半年运势好",
+                        "info" : "6个家庭清洁小技巧",
+                        "price" : "6.9",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/5a1df92d48fa3214bec9bb40ab067683.jpg"
+                    },
+                    {
+                        "auther" : "服装组：小服",
+                        "autherimg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15041772608140418.png",
+                        "tlt" : "小姐姐们的运动衣提前上架啦",
+                        "info" : "前两天推男式运动T恤时，就有小伙伴在专题评论里，问小姐姐们的运动衣在哪儿。",
+                        "price" : "",
+                        "img" : [
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15041772896010423.jpg",
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15041772808640420.jpg",
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15041772808640420.jpg"
+                        ]
+                    },
+                    {
+                        "auther" : "居家组：朵朵",
+                        "autherimg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15040896357740404.png",
+                        "tlt" : "初秋，最想用它来裸睡",
+                        "info" : "连续下了几场雨，杭州的早晚，已透出几丝凉意。再睡席子便有点凉了，于是周末从柜子翻...",
+                        "price" : "",
+                        "img" : [
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15040927525260414.jpg",
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15040927586650416.jpg",
+                             "https://cmljs.org/cml-demo/src/assets/images/yanxuan/15040927556820415.jpg"
+                        ]
+                    },
+                    {
+                        "auther" : "严选推荐",
+                        "autherimg" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/3d860cbf663253590da6a64ff07f9919.png",
+                        "tlt" : "不为繁华易匠心",
+                        "info" : "那些值得珍藏的严选手作好物",
+                        "price" : "29",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/4d72145e48e65ee3deaf2e1403e6ec73.jpg"
+                    }
+                ],
+                "classes" : [
+                    "推荐区",
+                    "家装区",
+                    "居家", 
+                    "餐厨", 
+                    "配件", 
+                    "服装", 
+                    "电器", 
+                    "洗护", 
+                    "杂货", 
+                    "饮食", 
+                    "婴童", 
+                    "志趣"
+                ],
+                "subclasses" : [
+                    {
+                        "name" : "中秋系列",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/82ae05c313b93355239ca1795917a5ac.png"
+                    },
+                    {
+                        "name" : "超值套装",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/bd6f7deba69c8af2f6bb80025d7b98de.png"
+                    },
+                    {
+                        "name" : "热卖爆品",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/c3418cc60d3968263c5b2ac7fb153c34.png"
+                    },
+                    {
+                        "name" : "999+好评",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/87d1cb1bc196c5775b17788aea1c2239.png"
+                    },
+                    {
+                        "name" : "boss推荐",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/fbee769af73c0f63f6120eb27ff3ce96.png"
+                    },
+                    {
+                        "name" : "明星推荐",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/7dea8f7e0e706804c3307504e2e7c463.png"
+                    },
+                    {
+                        "name" : "黑凤梨系列",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/a4a14669ce1fa497aece9a20c669196e.png"
+                    },
+                    {
+                        "name" : "趣味粉系列",
+                        "img" :  "https://cmljs.org/cml-demo/src/assets/images/yanxuan/87fc01e5876482d521ecca13aea42653.png"
+                    }
+                ]
+            }
+		}
+    }
