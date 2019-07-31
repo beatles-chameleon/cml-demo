94c89a683b02c4225eab5a8883aa42d1d72b1a68
JiM-W <550116990@qq.com>
2019-07-31 17:36:02 +0800

    增加web

---
+++ dist/alipay/static/js/pages/com/base/textarea/textarea.js
@@ -0,0 +1,1 @@
+var __CML__GLOBAL=require("../../../../manifest.js");__CML__GLOBAL.webpackJsonp([38],{239:function(e,t,u){u(240),u(241)},240:function(e,t){},241:function(e,t,u){Object.defineProperty(t,"__esModule",{value:!0});var n=u(0),a=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default={},t.default=a.default.createPage(t.default).getOptions()}},[239]);
--- dist/tt/pages/com/spread/c-picker/c-picker.js
+++
@@ -1,5 +0,0 @@
-var manifest = require('../../../../static/js/manifest.js');
-var cmldefine = manifest.cmldefine;
-var cmlrequire = manifest.cmlrequire;
-require('../../../../static/js/common.js');
-cmlrequire('513');
